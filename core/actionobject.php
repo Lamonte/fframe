0e72dc74d4467f390ecb099e697708eecf938667
Lamonte <scheols@gmail.com>
2012-04-22 16:13:39 -0500

    Updated readme and added license/copyright/version info to all files

--- core/actionobject.php
+++ core/actionobject.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * FFrame Forum Software
+ *
+ * LICENSE
+ * 
+ * The licenses for most software and other practical works are designed to take 
+ * away your freedom to share and change the works. By contrast, the GNU General 
+ * Public License is intended to guarantee your freedom to share and change all 
+ * versions of a program--to make sure it remains free software for all its users. 
+ * We, the Free Software Foundation, use the GNU General Public License for most 
+ * of our software; it applies also to any other work released this way by its 
+ * authors. You can apply it to your programs, too.
+ *
+ * @package	FFrame
+ * @copyright	Copyright (c) 2012 Lamonte & Helpers
+ * @license	http://www.opensource.org/licenses/lgpl-3.0.html GNU General Public License, version 3 (GPL-3.0)
+ * @version 	0.0.1 (Designating development stage http://en.wikipedia.org/wiki/Software_versioning)
+ */
+
 class ActionObject {
 
 	protected $data 	= null;
--- core/drivers/mysql.php
+++ core/drivers/mysql.php
@@ -1,11 +1,21 @@
 <?php
 /**
- * Database Driver Class that helps us connect to the database using
- * multiple database drivers.
+ * FFrame Forum Software
  *
- * @version 0.0.01b00001
- * @package fframe
- * @copyright 2012 Lamonte & FFrame Development Team
+ * LICENSE
+ * 
+ * The licenses for most software and other practical works are designed to take 
+ * away your freedom to share and change the works. By contrast, the GNU General 
+ * Public License is intended to guarantee your freedom to share and change all 
+ * versions of a program--to make sure it remains free software for all its users. 
+ * We, the Free Software Foundation, use the GNU General Public License for most 
+ * of our software; it applies also to any other work released this way by its 
+ * authors. You can apply it to your programs, too.
+ *
+ * @package	FFrame
+ * @copyright	Copyright (c) 2012 Lamonte & Helpers
+ * @license	http://www.opensource.org/licenses/lgpl-3.0.html GNU General Public License, version 3 (GPL-3.0)
+ * @version 	0.9.0.1 (Designating development stage http://en.wikipedia.org/wiki/Software_versioning)
  */
 
 class mysql_pdo {
